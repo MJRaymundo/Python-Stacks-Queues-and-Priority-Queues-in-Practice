6bff05fff614f499ec7c473986f864e31b961f98
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-12-12 22:03:18 +0800

    FIFO testing

--- queues.py
+++ queues.py
@@ -4,10 +4,24 @@ from collections import deque
 
 class Queue:
     def __init__(self):
-        self.elements = deque()
+        self._elements = deque()
 
     def enqueue(self, element):
         self._elements.append(element)
 
     def dequeue(self):
         return self._elements.popleft()
+# Testing FIFO queue
+
+#from queues import Queue
+
+#fifo = Queue()
+#fifo.enqueue("1st")
+#fifo.enqueue("2nd")
+#fifo.enqueue("3rd")
+
+#fifo.dequeue()
+
+#fifo.dequeue()
+
+#fifo.dequeue()
