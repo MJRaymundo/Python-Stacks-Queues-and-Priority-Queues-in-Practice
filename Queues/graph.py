d28209667fec6474becef4943f3af28733f80bd5
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-12-16 20:22:10 +0800

    Defined Retrace

--- Queues/graph.py
+++ Queues/graph.py
@@ -5,6 +5,7 @@
 from typing import NamedTuple
 import networkx as nx
 from queues import Queue
+from collections import deque
 
 #Object Representation of the Cities and Roads
 #Custom data type representing a city
@@ -69,3 +70,17 @@ def shortest_path(graph, source, destination, order_by= None):
                 previous[neighbor] == node
                 if neighbor == destination:
                     return retrace(previous, source, destination)
+
+#Defining Retrace
+def retrace(previous, source, destination):
+    path = deque()
+
+    current = destination
+    while current != source:
+        path.appendleft(current)
+        current = previous.get(current)
+        if current is None:
+            return None
+
+    path.appendleft(source)
+    return list(path)
