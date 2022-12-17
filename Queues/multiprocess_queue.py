91f379e17b7edf38668e2180465e1a3cf841bd4e
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2022-12-17 22:11:06 +0800

    Added Job dataclass

--- Queues/multiprocess_queue.py
+++ Queues/multiprocess_queue.py
@@ -5,6 +5,7 @@ from hashlib import md5
 from itertools import product
 from string import ascii_lowercase
 import multiprocessing
+from dataclasses import dataclass
 
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
     for length in range(1, max_length + 1):
@@ -66,3 +67,17 @@ class Worker(multiprocessing.Process):
             if plaintext := job(self.hash_value):
                 self.queue_out.put(plaintext)
                 break
+
+#Added Job dataclass
+@dataclass(frozen=True)
+class Job:
+    Combinations: Combinations
+    start_index: int
+    stop_index: int
+
+    def __call__(self, hash_value):
+        for index in range(self.start_index, self.stop_index):
+            text_bytes = self.combinations[index].encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
